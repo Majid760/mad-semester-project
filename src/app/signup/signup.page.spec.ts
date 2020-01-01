2ff14563164211aefc0d92c54b78f1392f946b32
Majid760 <55375676+Majid760@users.noreply.github.com>
2020-01-02 04:12:56 +0500

    Signup pages functionalities are added

--- src/app/signup/signup.page.spec.ts
+++ src/app/signup/signup.page.spec.ts
@@ -3,25 +3,25 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupPage } from './signup.page';
 
-// describe('SignupPage', () => {
-//   let component: SignupPage;
-//   let fixture: ComponentFixture<SignupPage>;
+describe('SignupPage', () => {
+  let component: SignupPage;
+  let fixture: ComponentFixture<SignupPage>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ SignupPage ],
-//       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SignupPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(SignupPage);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignupPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
